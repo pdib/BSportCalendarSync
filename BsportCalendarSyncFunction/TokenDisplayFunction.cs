042a0795a98702d4f578d4aae8590f85ff086181
Paul Dib <padib@microsoft.com>
2022-04-19 22:19:13 +0200

    Add token display

---
+++ BsportCalendarSyncFunction/TokenDisplayFunction.cs
@@ -0,0 +1,21 @@
+﻿namespace BsportCalendarSyncFunction
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.Extensions.Logging;
+
+    public class TokenDisplayFunction
+    {
+        [FunctionName("oauth-fetcher")]
+        public IActionResult DisplayToken([HttpTrigger(AuthorizationLevel.Anonymous, "GET", "/api/oauth-fetcher")]HttpRequest request, ILogger log)
+        {
+            log.LogInformation("Receiving token from auth.");
+
+            string query = request.QueryString.Value;
+
+            return new OkObjectResult(query);
+        }
+    }
+}
